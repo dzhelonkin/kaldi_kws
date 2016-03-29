aa49955a115ecbb988a31ce85515bb99a7145035
artsokol <artsokol87@gmail.com>
2016-03-29 23:02:23 +0300

    gecode solver

--- Coloring/VCPSolver.cc
+++ Coloring/VCPSolver.cc
@@ -43,9 +43,28 @@ int main(int argc, char **argv)
         cout << "Please specify DIMACS format file as argument" << endl;
         return -1;
     }
-    
+
     VCPSolver solver(argv[1]);
 
+
+    SizeOptions opt("GraphColor");
+    opt.icl(ICL_DOM);
+    opt.iterations(20);
+    opt.solutions(0);
+    //opt.model(GraphColor::MODEL_NONE);
+    //opt.model(GraphColor::MODEL_NONE, "none", "no lower bound");
+    opt.model(GraphColor::MODEL_CLIQUE, "clique", "use maximal clique size as lower bound");
+    //opt.branching(GraphColor::BRANCH_DEGREE);
+    opt.branching(GraphColor::BRANCH_DEGREE, "degree");
+    opt.branching(GraphColor::BRANCH_SIZE, "size");
+    opt.branching(GraphColor::BRANCH_SIZE_DEGREE, "sizedegree");
+    opt.branching(GraphColor::BRANCH_SIZE_AFC, "sizeafc");
+    opt.branching(GraphColor::BRANCH_SIZE_ACTIVITY, "sizeact");
+    opt.symmetry(GraphColor::SYMMETRY_NONE);
+    opt.symmetry(GraphColor::SYMMETRY_NONE,"none");
+    //opt.symmetry(GraphColor::SYMMETRY_LDSB,"ldsb","use value symmetry breaking");
+    //opt.parse(argc,argv);
+    Script::run<GraphColor,BAB,SizeOptions>(opt);
 
     return 0;
 }
